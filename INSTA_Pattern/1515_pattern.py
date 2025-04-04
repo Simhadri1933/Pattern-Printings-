3f7f88d0824e3c98ef644c9991c5aa517bef735a
Simhadri <162089079+Simhadri1933@users.noreply.github.com>
2025-04-04 21:19:08 +0530

    Add files via upload

---
+++ INSTA_Pattern/1515_pattern.py
@@ -0,0 +1,26 @@
+"""
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+  * * * * * * * 
+    * * * * * 
+      * * * 
+        * 
+"""
+
+
+n=5
+for i in range(1,n+1):
+    for space in range(1,n-i+1):
+        print(" ",end=" ")
+    for j in range(1,2*i):
+        print("*",end=" ")
+    print()
+for i in range(1,n):
+    for space in range(1,i+1):
+        print(" ",end=" ")
+    for j in range(1,2*n-2*i):
+        print("*",end=" ")
+    print()
